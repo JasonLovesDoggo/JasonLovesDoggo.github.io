6f4ec77320eda4a536e869b18e5fec40b82d72a2
Jason <thebosssg19@gmail.com>
2022-02-11 18:30:55 -0500

    nightly update 13A2M1D working on bella's page and the Generategallary.py file behind it

--- bella/Generategallary.py
+++ bella/Generategallary.py
@@ -1,2 +1,30 @@
-import os
-import glob
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+path = 'assets/Img/Bella'
+
+template = """
+            div>
+                <img data-u="image" src="{image}"  alt="{alt}"/>
+            </div>
+
+"""
+
+
+def add_metadata(img, alt: str):
+    # read the image data using PIL
+    image = Image.open(img)
+    exifdata = image.getexif()
+    # iterating over all EXIF data fields
+    for tag_id in exifdata:
+        # get the tag name, instead of human unreadable tag id
+        tag = TAGS.get(tag_id, tag_id)
+        data = exifdata.get(tag_id)
+        # decode bytes
+        if isinstance(data, bytes):
+            data = data.decode()
+        print(f"{tag:25}: {data}")
+
+
+def gen_img(img: str, alt: str):
+    pass
