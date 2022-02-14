e97c545e59779cf56b9f050e8590ded3387a37e1
Jason <thebosssg19@gmail.com>
2022-02-13 22:08:42 -0500

    Nightly update #2 4m just the word list updated before i go to bed

--- foodle/generate words/concatonate the lists and remove dupes.py
+++ foodle/generate words/concatonate the lists and remove dupes.py
@@ -1,3 +1,23 @@
+import json
+
+with open('known_5_letter_words.json', 'r') as known_five_letter_words_file:
+    known_five_letter_words = json.load(known_five_letter_words_file)
+
+with open('non_food_words.json', 'r') as non_food_words_file:
+    non_food_words = json.load(non_food_words_file)
 
 
-mylist = list(dict.fromkeys(mylist))
+with open('food_words.json', 'r') as food_words_file:
+    food_words = json.load(food_words_file)
+
+all_5_letter_words = non_food_words + known_five_letter_words
+
+for word in all_5_letter_words:
+    if word in food_words:
+        print(word)
+        x = all_5_letter_words.pop(word)
+        print(x)
+
+
+valid_guesses = list(dict.fromkeys(all_5_letter_words))
+print(f'{valid_guesses}')
