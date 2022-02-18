c5955b885b1b5ef84d54a2fe7572cbbb0eb733e8
Jason <thebosssg19@gmail.com>
2022-02-18 15:44:05 -0500

    1m still had debug code

--- bella/tempdir/scripts/Generategallary.py
+++ bella/tempdir/scripts/Generategallary.py
@@ -19,7 +19,6 @@ dir = str(os.path.abspath(__file__))[:-41]
 def gen_photo_list(subdir: str = ''):
     list_of_photos = []
     for img in listdir(f'{dir}/assets/Img/Bella{subdir}'):
-        print(subdir)
         list_of_photos.append(f'{dir_list}{subdir[1:]}{"/" if subdir != "" else ""}{img}')
     return list_of_photos
 
