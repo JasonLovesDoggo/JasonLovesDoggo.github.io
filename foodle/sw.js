62495dbcfa7ee9ad9736534da2a28d01744bdb4a
Jason <thebosssg19@gmail.com>
2022-02-20 20:35:57 -0500

    or did I.. (refer to prev commit)
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- foodle/sw.js
+++ foodle/sw.js
@@ -1,13 +1,11 @@
-// Increase the v number when the app is updated
-const cacheName = "foodle-v1.1.2";
 //https://medium.com/notonlycss/turn-your-website-into-a-pwa-6aaecb95f8b9
+const dynamicCacheName = 'foodle-v1.1.2';
 const assetsToCache = [
 	"./",
 	"./index.html",
 	"./src/build/bundle.css",
 	"./src/build/bundle.js"
 ];
-
 self.addEventListener("install", e => {
 	log("Installing");
 	e.waitUntil((async () => {
@@ -15,32 +13,32 @@ self.addEventListener("install", e => {
 		await cache.addAll(assetsToCache);
 	})());
 });
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key =>  key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
 
-self.addEventListener("fetch", e => {
-	log("Fetching definition");
-	e.respondWith((async () => {
-		const r = await caches.match(e.request);
-		if (r) {
-			log("Definition found in cache");
-			return r;
-		};
-		const response = await fetch(e.request);
-		const cache = await caches.open("defs");
-		log("Caching definition");
-		cache.put(e.request, response.clone());
-		return response;
-	})());
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone()).then(r =>  log('Caching Url ${evt.request.url}'));
+          return fetchRes;
+        })
+      });
+    })
+  );
 });
-// When the name of the cache changes (because the app has been updated) delete the old cache
-self.addEventListener("activate", e => {
-	e.waitUntil(caches.keys().then(keys => {
-		return Promise.all(keys.map((key) => {
-			if (key === cacheName || key === "defs") return;
-			log(`Cache updated to ${cacheName.split("-")[1]}, old cache deleted`);
-			return caches.delete(key);
-		}));
-	}));
-});
+
 /**
  * Special log function to clearly distinguish logs from the service worker.
  * @param {string} text - The text to log
