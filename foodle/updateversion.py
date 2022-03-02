1f6591a6972ae69b8e3dc6c3fc37bf67c1cd94f7
Jason <thebosssg19@gmail.com>
2022-03-01 23:02:09 -0500

    Fix: version num was off
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- foodle/updateversion.py
+++ foodle/updateversion.py
@@ -1,11 +1,11 @@
 from os.path import abspath
 
 # File paths
-old_version = 1.51
+old_version = 1.5
 
 dir = str(abspath(__file__))[:-16]
 fps = [f'{dir}index.html', f'{dir}sw.js', f'{dir}package.json', f'{dir}src/main.ts', f'{dir}updateversion.py']
-#  "version": "1.27",
+#  "version": "1.5",
 
 for file in fps:
     print(file)
@@ -17,4 +17,5 @@ for file in fps:
 
     # Write the file out again
     with open(file, 'w') as file_d2:
+        print(filedata)
         file_d2.write(filedata)
