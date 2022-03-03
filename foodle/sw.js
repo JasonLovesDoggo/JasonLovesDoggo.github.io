476878b97e6dc78710a6ed283c16c9425f229203
Jason | Version Updater <thebosssg19@gmail.com>
2022-03-03 02:13:58 +0000

    Update gallary file

--- foodle/sw.js
+++ foodle/sw.js
@@ -1,5 +1,5 @@
 //https://medium.com/notonlycss/turn-your-website-into-a-pwa-6aaecb95f8b9
-const dynamicCacheName = 'Img-v1.51';
+const dynamicCacheName = 'Img-v1.52';
 const assetsToCache = [
 	"./",
 	"./index.html",
