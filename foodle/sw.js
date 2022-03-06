b94b85d4d4045a78a57bb3978101a5bb65902165
Jason | Version Updater <thebosssg19@gmail.com>
2022-03-06 01:46:47 +0000

    Update Foodle Version

--- foodle/sw.js
+++ foodle/sw.js
@@ -1,5 +1,5 @@
 //https://medium.com/notonlycss/turn-your-website-into-a-pwa-6aaecb95f8b9
-const dynamicCacheName = 'Img-v1.59';
+const dynamicCacheName = 'Img-v1.6';
 const assetsToCache = [
 	"/foodle",
 	"/foodle/index.html",
