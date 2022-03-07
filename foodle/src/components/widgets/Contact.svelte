06995ff16c9ef2dba2186f69290134d24423d309
Jason <thebosssg19@gmail.com>
2022-03-06 22:55:26 -0500

    The form will be implemented tomorrow
    WebStorm isn't responding so yelp
    
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- foodle/src/components/widgets/Contact.svelte
+++ foodle/src/components/widgets/Contact.svelte
@@ -2,81 +2,90 @@
 
 </script>
 
-<h1>Contact Me</h1>
-<div>
-	After each guess, the color of the tiles will change to show how close your guess was to the
-	word.
-</div>
-<form id="fs-frm" name="department-contact-form" accept-charset="utf-8" action="https://formspree.io/f/xqknbywk" method="post">
-  <fieldset id="fs-frm-inputs">
-    <div class="g-recaptcha" data-sitekey="6LexoLkeAAAAAAeeRiBX2wFK7nQ1eaW68mRS2J1m"></div>
-    <label for="full-name">Full Name</label>
-    <input type="text" name="name" id="full-name" placeholder="First and Last" required="">
-    <label for="email-address">Email Address</label>
-    <input type="email" name="email" id="email-address" placeholder="email@domain.tld" required="">
-	<input type="tel" name="telephone" id="telephone" placeholder="(555) 555-5555">
-    <label for="Issue-Type">Issue Type</label>
-    <select name="department" id="Issue-Type" required="">
+<div class="container">
+    <h1>Contact Me</h1>
+     <form name="Foodle-form" accept-charset="utf-8" action="https://formspree.io/f/xqknbywk" method="post">
+              <!-- <div class="g-recaptcha" data-sitekey="6LexoLkeAAAAAAeeRiBX2wFK7nQ1eaW68mRS2J1m"></div>  -->
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="First name" placeholder="Jason" required>
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="Fast name" placeholder="Cameron " required>
+
+    <label for="email">Email</label>
+    <input type="email" id="email" name="Email" placeholder="foodlethegame@gmail.com" required>
+    <label for="Issue-Type"> Issue Type</label>
+         <select name="Issue Type" id="Issue-Type" required="">
       <option value="" selected="" disabled="">Select</option>
       <option value="Bug Report">Bug Report</option>
       <option value="Missing Word">Missing Word</option>
-      <option value="Shipping Receiving">Shipping / Receiving</option>
-      <option value="Billing Accounting">Billing / Accounting</option>
+      <option value="Other">Other</option>
     </select>
-    <label for="message">Message</label>
-    <textarea rows="5" name="message" id="message" placeholder="Put your message here." required=""></textarea>
-    <input type="hidden" name="_subject" id="email-subject" value="Foodle Form Submission">
-  </fieldset>
-  <input type="submit" value="Send Message">
-  <input type="reset" value="Reset form">
-</form><style>
-#fs-frm input,
-#fs-frm select,
-#fs-frm textarea,
-#fs-frm fieldset,
-#fs-frm label,
-#fs-frm label,
-#fs-frm ::placeholder {
-  font-size: .825rem;
-  margin-bottom: .5rem;
-  padding-top: .2rem;
-  display: flex;
-  align-items: baseline;
+
+    <label for="message">Subject</label>
+    <textarea id="message" name="message" placeholder="Write something.." rows="2" style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+    <input type="reset" value="Reset">
+
+  </form>
+</div>
+
+<style>
+  input[type=text], input[type=email], select, textarea{
+  color: var(--bg-primary);
+  width: 100%; /* Full width */
+  padding: 12px; /* Some padding */
+  border: 1px solid #ccc; /* Gray border */
+  border-radius: 4px; /* Rounded borders */
+  box-sizing: border-box; /* Make sure that padding and width stays in place */
+  margin-top: 6px; /* Add a top margin */
+  margin-bottom: 16px; /* Bottom margin */
+  resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+}
+ ::placeholder, ::-ms-input-placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: var(--fg-primary);
+  opacity: 1; /* Firefox */
 }
 
-/* border, padding, margin, width */
-#fs-frm input,
-#fs-frm select,
-#fs-frm textarea,
-#fs-frm input:focus,
-#fs-frm select:focus,
-#fs-frm textarea:focus {
-  background-color: white;
-  outline: gray solid thin;
-  outline-offset: -1px;
-}
-#fs-frm [type="text"],
-#fs-frm [type="email"] {
-  width: 100%;
-}
-#fs-frm [type="submit"],
-#fs-frm [type="reset"] {
-  width: auto;
+  label {
+      color: var(--fg-primary);
+  }
+/* Style the submit button with a specific background color etc */
+input[type=submit], input[type=reset] {
+  background-color: var(--fg-primary);
+  color: var(--bg-primary);            /* This works */
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
   cursor: pointer;
-  -webkit-appearance: button;
-  -moz-appearance: button;
-}
-#fs-frm [type="submit"]:focus,
-#fs-frm [type="reset"]:focus {
-  outline: none;
-}
-#fs-frm [type="submit"],
-#fs-frm [type="reset"] {
-  margin-bottom: 0;
-}
-#fs-frm select {
-  text-transform: none;
 }
 
+input:checked {
+  color: var(--fg-primary);
+}
 
+/* When moving the mouse over the submit button, add a darker green color */
+input[type=submit]:hover {
+  background-color: var(--color-correct);
+}
+
+input[type=reset]:hover {
+    background-color: var(--red);
+}
+/* Add a background color and some padding around the form */
+.container {
+    /* From https://css.glass */
+        background: rgba(var(--bg-primary), 0.5);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(0px);
+    -webkit-backdrop-filter: blur(0px);
+    margin: 25px;
+    padding: 32px;
+}
+.container h1 {
+    color: var(--fg-primary);
+}
 </style>
+
