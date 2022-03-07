2218f72345031395e82e21ed77d6e12b390664cd
Jason <thebosssg19@gmail.com>
2022-03-07 17:53:15 -0500

    Fixed updateversion.py so i never get a number like 1.620000000000001
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- foodle/updateversion.py
+++ foodle/updateversion.py
@@ -1,19 +1,38 @@
 from os.path import abspath
 
 # File paths
-old_version = 1.62
 
-dir = str(abspath(__file__))[:-16]
-fps = [f'{dir}index.html', f'{dir[:-7]}foodle_sw.js', f'{dir}package.json', f'{dir}src/main.ts', f'{dir}updateversion.py']
-#  "version": "1.62",
+old_version_decimal = 63
+VERSION_NUMBER = 1
+new_version = f'{VERSION_NUMBER}.{old_version_decimal + 1}'
+old_version = f'{VERSION_NUMBER}.{old_version_decimal}'
+
+print('Old Version:', old_version)
+print('New Version:', new_version)
+
+direct = str(abspath(__file__))[:-16]
+fps = [f'{direct}index.html', f'{direct[:-7]}foodle_sw.js', f'{direct}package.json', f'{direct}src/main.ts',
+       f'{direct}updateversion.py']
+#  "version": "1.63",
+
 
 for file in fps:
-    print(file)
+    print(file, ': Modified')
     # Read in the file
     with open(file, 'r') as file_d1:
         filedata = file_d1.read()
     # Replace the target string
-    filedata = filedata.replace(str(old_version), str(old_version + 0.01))
+    if old_version_decimal == 99:
+        filedata = filedata.replace(f'VERSION_NUMBER = {VERSION_NUMBER}', f'VERSION_NUMBER = {VERSION_NUMBER + 1}')
+        filedata = filedata.replace(f'old_version_decimal = {old_version_decimal}',
+                                    f'old_version_decimal = {"0"}')
+        filedata = filedata.replace(old_version, new_version)
+
+    else:
+        filedata = filedata.replace(f'old_version_decimal = {old_version_decimal}',
+                                    f'old_version_decimal = {old_version_decimal + 1}')
+
+        filedata = filedata.replace(old_version, new_version)
 
     # Write the file out again
     with open(file, 'w') as file_d2:
