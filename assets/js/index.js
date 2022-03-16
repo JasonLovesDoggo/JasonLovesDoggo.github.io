c1bc59e6272b183a90146e47e6d8bfc79c282280
Jason <thebosssg19@gmail.com>
2022-03-16 19:15:59 -0400

    Added a Home option on the color picker and a responsive sidebar on the main page i.e navigation menu on main page and a way to go back home on the color picker
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,48 @@
-function showcontact(){
-    $("#contact_container").css("display","inherit").addClass("animated slideInUp");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideInUp");
-    }, 500);}
-function closecontact(){
-    $("#contact_container").addClass("animated slideOutDown");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideOutDown").css("display","none");
-    },500);}
+const sidebarBox = document.querySelector('#bar_box'),
+  sidebarBtn = document.querySelector('#bar_btn'),
+  pageWrapper = document.querySelector('#page-wrapper');
+
+sidebarBtn.addEventListener('click', (event) => {
+  sidebarBtn.classList.toggle('active');
+  sidebarBox.classList.toggle('active');
+});
+
+pageWrapper.addEventListener('click', (event) => {
+  if (sidebarBox.classList.contains('active')) {
+    sidebarBtn.classList.remove('active');
+    sidebarBox.classList.remove('active');
+  }
+});
+
+window.addEventListener('keydown', (event) => {
+  if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
+    sidebarBtn.classList.remove('active');
+    sidebarBox.classList.remove('active');
+  }
+});
+
+
+function showcontact() {
+  $('#contact_container')
+    .css('display', 'inherit')
+    .addClass('animated slideInUp');
+  setTimeout(function () {
+    $('#contact_container')
+      .removeClass('animated slideInUp');
+  }, 500);
+}
+
+function closecontact() {
+  $('#contact_container')
+    .addClass('animated slideOutDown');
+  setTimeout(function () {
+    $('#contact_container')
+      .removeClass('animated slideOutDown')
+      .css('display', 'none');
+  },500);}
 
 $(document).ready(function() {
-    setTimeout(function(){
+  setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
     setTimeout(function(){
       $("#loading").removeClass("animated fadeOut").css("display","none");
@@ -19,5 +51,5 @@ $(document).ready(function() {
       $("#contact").removeClass("animated fadeIn");
       $("#projects").removeClass("animated fadeIn");
     },700);
-    },1200);
+  },1200);
 });
