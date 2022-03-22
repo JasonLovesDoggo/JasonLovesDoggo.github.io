133b40f4ed9625d8d78221552983b8aeed302065
Jason <thebosssg19@gmail.com>
2022-03-21 20:55:47 -0400

    2a11m - Updated word list added like 20 words current total is 172 words - updated logo its dope now (not really but better than before)
    TODO add a FAQ in the Tutorial. Svelte file it's a WIP rn
    
    have the colors set to varibles, so it can always be updated
    
    TO BE FINISHED IN NEXT COMMIT
    
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- foodle/generate words/concatonate the lists and remove dupes.py
+++ foodle/generate words/concatonate the lists and remove dupes.py
@@ -14,8 +14,58 @@ not_really_food_words = ['heath', 'stink', 'grade', 'unfed', 'pound', 'belly', '
                          'stank', 'acorn', 'snail', 'poppy', 'moldy', 'molar', 'musky', 'fluid', 'hazel', 'leafy',
                          'fatty', 'conch', 'savoy', 'eater', 'butch']
 
-actually_food_words_that_dont_appear_in_the_dictionary_i_used = []  #empty rn
-
+actually_food_words_that_dont_appear_in_the_dictionary_i_used = [
+    'oreos',
+    'heinz',
+    'bhaji',
+    'torte',
+    'crisp',
+    'chips',
+    'anise',
+    'punch',
+    'tuber',
+    'ugali',
+    'ladle',
+    'beans',
+    'spuds',
+    'stove',
+    'spork',
+    'herbs',
+    'lager',
+    'seeds',
+    'liver',
+    'namul',
+    'lassi',
+    'ranch',
+    'cater',
+    'manti',
+    'straw',
+    'grain',
+    'crabs',
+    'jelly',
+    'adobo',
+    'taste',
+    'pilaf',
+    'mochi',
+    'vegan',
+    'latke',
+    'munch',
+    'queso',
+    'curds',
+    'roast',
+    'fries',
+    'chard',
+    'mints',
+    'minty',
+    'dates',
+    'clams',
+    'prune',
+    'aspic',
+    'rujak',
+    'gummy',
+    'cakes',
+    'baozi',
+]
 for word in not_really_food_words:
     food_words.remove(word)
 
@@ -23,7 +73,6 @@ for word in actually_food_words_that_dont_appear_in_the_dictionary_i_used:
     food_words.append(word)
 
 # Fix guess list
-
 guesses_words.append('urmom')  # your welcome Zack
 guesses_words.append('soare')  # your welcome Mason
 
@@ -42,3 +91,21 @@ x = f"""const words =
 export default words;"""""
 with open('../src/words_5.ts', 'w') as word_ts_file:
     word_ts_file.write(x)
+
+x = ['ALl words',
+    ['fresh', 'crust', 'serve', 'flesh', 'seedy', 'corny', 'salad', 'spicy', 'kebab', 'pulpy', 'vodka', 'peach',
+     'drink', 'tangy', 'sugar', 'aroma', 'shake', 'spill', 'brine', 'sweet', 'mince', 'olive', 'zesty', 'scrap',
+     'apron', 'cacao', 'bland', 'quart', 'smear', 'patty', 'hunky', 'treat', 'fungi', 'booze', 'thyme', 'grate',
+     'soggy', 'enjoy', 'mocha', 'smoke', 'lunch', 'paste', 'carve', 'apple', 'honey', 'cream', 'diner', 'onion',
+     'juicy', 'icing', 'eaten', 'whisk', 'sushi', 'candy', 'grape', 'burnt', 'dough', 'trout', 'filet', 'donut',
+     'bread', 'dairy', 'tasty', 'puree', 'diary', 'penne', 'ramen', 'fishy', 'steak', 'caper', 'pizza', 'plate',
+     'broil', 'cumin', 'gravy', 'yeast', 'cider', 'creme', 'grill', 'flour', 'beefy', 'scone', 'toast', 'saucy',
+     'spice', 'feast', 'syrup', 'maize', 'cocoa', 'mango', 'chili', 'salty', 'pesto', 'salsa', 'spoon', 'crepe',
+     'water', 'briny', 'bagel', 'melon', 'pasta', 'taffy', 'basil', 'wafer', 'snack', 'bacon', 'juice', 'wheat',
+     'berry', 'fruit', 'sauce', 'fried', 'baste', 'pecan', 'curry', 'lemon', 'latte', 'broth', 'guava', 'fudge',
+     'femur', 'gourd', 'oreos', 'heinz', 'bhaji', 'torte', 'crisp', 'chips', 'anise', 'punch', 'tuber', 'ugali',
+     'ladle', 'beans', 'spuds', 'stove', 'spork', 'herbs', 'lager', 'seeds', 'liver', 'namul', 'lassi', 'ranch',
+     'cater', 'manti', 'straw', 'grain', 'crabs', 'jelly', 'adobo', 'taste', 'pilaf', 'mochi', 'vegan', 'latke',
+     'munch', 'queso', 'curds', 'roast', 'fries', 'chard', 'mints', 'minty', 'dates', 'clams', 'prune', 'aspic',
+     'rujak', 'gummy', 'cakes', 'baozi']]
+
