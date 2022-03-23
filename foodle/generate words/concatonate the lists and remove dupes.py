eaf857b863dc38447984b3b6af6f883de513d19d
Jason <thebosssg19@gmail.com>
2022-03-22 23:02:17 -0400

    all thats left is to make the logo brighter
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- foodle/generate words/concatonate the lists and remove dupes.py
+++ foodle/generate words/concatonate the lists and remove dupes.py
@@ -12,7 +12,7 @@ not_really_food_words = ['heath', 'stink', 'grade', 'unfed', 'pound', 'belly', '
                          'cheek', 'plant', 'hasty', 'trash', 'larva', 'hairy', 'tipsy', 'delve', 'glass', 'fluff',
                          'stalk', 'liver', 'cinch', 'waste', 'stein', 'vomit', 'baker', 'crave', 'opium', 'derby',
                          'stank', 'acorn', 'snail', 'poppy', 'moldy', 'molar', 'musky', 'fluid', 'hazel', 'leafy',
-                         'fatty', 'conch', 'savoy', 'eater', 'butch']
+                         'fatty', 'conch', 'savoy', 'eater', 'butch', 'eaten']
 
 actually_food_words_that_dont_appear_in_the_dictionary_i_used = [
     'oreos',
@@ -91,21 +91,3 @@ x = f"""const words =
 export default words;"""""
 with open('../src/words_5.ts', 'w') as word_ts_file:
     word_ts_file.write(x)
-
-x = ['ALl words',
-    ['fresh', 'crust', 'serve', 'flesh', 'seedy', 'corny', 'salad', 'spicy', 'kebab', 'pulpy', 'vodka', 'peach',
-     'drink', 'tangy', 'sugar', 'aroma', 'shake', 'spill', 'brine', 'sweet', 'mince', 'olive', 'zesty', 'scrap',
-     'apron', 'cacao', 'bland', 'quart', 'smear', 'patty', 'hunky', 'treat', 'fungi', 'booze', 'thyme', 'grate',
-     'soggy', 'enjoy', 'mocha', 'smoke', 'lunch', 'paste', 'carve', 'apple', 'honey', 'cream', 'diner', 'onion',
-     'juicy', 'icing', 'eaten', 'whisk', 'sushi', 'candy', 'grape', 'burnt', 'dough', 'trout', 'filet', 'donut',
-     'bread', 'dairy', 'tasty', 'puree', 'diary', 'penne', 'ramen', 'fishy', 'steak', 'caper', 'pizza', 'plate',
-     'broil', 'cumin', 'gravy', 'yeast', 'cider', 'creme', 'grill', 'flour', 'beefy', 'scone', 'toast', 'saucy',
-     'spice', 'feast', 'syrup', 'maize', 'cocoa', 'mango', 'chili', 'salty', 'pesto', 'salsa', 'spoon', 'crepe',
-     'water', 'briny', 'bagel', 'melon', 'pasta', 'taffy', 'basil', 'wafer', 'snack', 'bacon', 'juice', 'wheat',
-     'berry', 'fruit', 'sauce', 'fried', 'baste', 'pecan', 'curry', 'lemon', 'latte', 'broth', 'guava', 'fudge',
-     'femur', 'gourd', 'oreos', 'heinz', 'bhaji', 'torte', 'crisp', 'chips', 'anise', 'punch', 'tuber', 'ugali',
-     'ladle', 'beans', 'spuds', 'stove', 'spork', 'herbs', 'lager', 'seeds', 'liver', 'namul', 'lassi', 'ranch',
-     'cater', 'manti', 'straw', 'grain', 'crabs', 'jelly', 'adobo', 'taste', 'pilaf', 'mochi', 'vegan', 'latke',
-     'munch', 'queso', 'curds', 'roast', 'fries', 'chard', 'mints', 'minty', 'dates', 'clams', 'prune', 'aspic',
-     'rujak', 'gummy', 'cakes', 'baozi']]
-
