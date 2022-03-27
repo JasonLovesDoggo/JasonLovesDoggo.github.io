119d15d00f18f0dd54663e62945e46b6bca8809e
Jason <thebosssg19@gmail.com>
2022-03-27 15:01:47 -0400

    "self-updating changelog",     "Added 25+ food words totaling 171 total possible answers",     "Changed Header's Color",     "More clear wording in some forms and other bits and bytes including the tips",     "Bug fixes: internal fixes, a bug where the word would reset early",     "re-built the game"
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- foodle/src/components/widgets/ChangeLog.svelte
+++ foodle/src/components/widgets/ChangeLog.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    import SvelteMarkdown from 'svelte-markdown'
 
     export let visible: boolean;
     let source: Response | string = ` # Fetching ChangeLog...`
     let filename: string
     let githuburl: string
-    import { onMount } from 'svelte';
-
-	onMount(() => {
-        GetLatestVersion()
-		//setInterval(GetLatestVersion, 86400000) //Check the latest version once a day
-	});
-    /** The maximum number of alternate definitions to provide*/
+    let json: Record<string, string | []> = {};
+    let errormsg: string
+    //import { onMount } from 'svelte';
+            //TODO: redo this module with the foodle api
+    //onMount(() => {
+    //setInterval(GetLatestVersion, 86400000) //Check the latest version once a day
+    //});
 
     async function GetLatestVersion() {    // TODO: change this to the foodle api when ready
         const data = await fetch(`https://api.github.com/repos/JasonLovesDoggo/JasonLovesDoggo.github.io/releases/latest`, {
@@ -20,33 +19,65 @@
         let json = (await data.json())
         if (data.ok) {
             githuburl = json['html_url']
-             console.log(`Version: ${json['tag_name']}`)
-            return source = await GetChangeLog(json['tag_name']);
-        } else {
-            return source = ` # Failed to fetch Changelog`
-
+            return await GetChangeLog(json['tag_name']);
         }
+
     }
+
     async function GetChangeLog(tag_name) {    // TODO: change this to the foodle api when ready
-        const data = await fetch(`https://nasoj.me/foodle/changelogs/${tag_name}.md`, {
+        const data = await fetch(`https://nasoj.me/foodle/changelogs/${tag_name}.json`, {
             cache: 'no-cache'
         });
         if (data.ok) {
-            return data.text();
+            return json = await data.json();
         } else {
-            return source = ` # Failed to fetch Changelog`
+            throw new Error('Failed to fetch the changelog')
 
         }
     }
-
+    //TODO make the changelog scrollable via {#each and loop through the
 </script>
 
 <div class:complete={visible} id="ChangeLogContainer">
-    <SvelteMarkdown {source} />
-    <h2><a target="_blank" href={githuburl}>View on GitHub</a></h2>
+    {#await GetLatestVersion()}
+        <h1>Fetching data...</h1>
+    {:then json}
+        {#if errormsg !== ''}
+
+            <h1 style="padding-bottom: 7%;" class="center">Foodle {json['version']}</h1>
+            <h2>{json['title']}</h2>
+            <ul class="margind">
+                {#each json['changes'] as change}
+                    <li class="margind">{change}</li>
+                {/each}
+            </ul>
+            <h2 style="padding-top: 4%;" class="center"><a target="_blank" href={githuburl}>View on GitHub</a>
+            </h2>
+        {/if}
+
+    {:catch s}
+        <div><h2> Failed to fetch the changelog but you can <a target="_blank" href={githuburl}>view it on GitHub</a>
+        </h2></div>
+    {/await}
+
 </div>
 
 
 <style>
-    #ChangeLogContainer {margin: 5%;}
+    *:before, *:after {
+        margin-bottom: 5px;
+    }
+
+    .center {
+        margin: auto;
+        width: 50%;
+    }
+
+    .margind {
+        margin: 15px;
+    }
+    #ChangeLogContainer {
+        position: relative;
+        margin: 5%;
+    }
 </style>
