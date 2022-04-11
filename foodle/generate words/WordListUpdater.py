a67643bebbd5fc638215a1ace0016b6309dbc90a
Jason <thebosssg19@gmail.com>
2022-04-11 17:41:26 -0400

    Foodle v 1.88 added umami and mirin to the food list! Thanks to https://twitter.com/baby_chicane/status/1513432295042129922 and https://twitter.com/janabakunina/status/1513431897166200834
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- foodle/generate words/WordListUpdater.py
+++ foodle/generate words/WordListUpdater.py
@@ -15,6 +15,8 @@ not_really_food_words = ['heath', 'stink', 'grade', 'unfed', 'pound', 'belly', '
                          'fatty', 'conch', 'savoy', 'eater', 'butch', 'eaten']
 
 actually_food_words_that_dont_appear_in_the_dictionary_i_used = [
+    'umami',
+    'mirin',
     'rolls',
     'gumbo',
     'beets',
--- foodle/updateversion.py
+++ foodle/updateversion.py
@@ -2,7 +2,7 @@ from os.path import abspath
 
 # File paths
 
-old_version_decimal = 86
+old_version_decimal = 88
 VERSION_NUMBER = 1
 new_version = f'{VERSION_NUMBER}.{old_version_decimal + 1}'
 old_version = f'{VERSION_NUMBER}.{old_version_decimal}'
@@ -12,8 +12,8 @@ print('New Version:', new_version)
 
 direct = str(abspath(__file__))[:-16]
 fps = [f'{direct}index.html', f'{direct[:-7]}foodle_sw.js', f'{direct}package.json', f'{direct}src/main.ts',
-       f'{direct}updateversion.py', f'{direct[:-7]}api\\json-data\\version.json']
-#  "version": "1.86",
+       f'{direct}updateversion.py', f'{direct[:-7]}api\\data\\version.json']
+#  "version": "1.88",
 
 
 for file in fps:
