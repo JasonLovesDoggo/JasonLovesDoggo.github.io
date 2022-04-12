4c2b7af56d57753cfcd8df69e0afe1120f747888
Jason <thebosssg19@gmail.com>
2022-04-12 19:59:55 -0400

    improved caching with the Definition.svelte module
    Signed-off-by: Jason <thebosssg19@gmail.com>

--- frenchle/src/enums.ts
+++
@@ -1,5 +0,0 @@
-export enum GameMode {
-	"daily",
-	"hourly",
-	"infinite",
-}
--- frenchle/src/stores.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-import type { GameMode } from "./enums";
-import { createDefaultSettings, createLetterStates } from "./utils";
-
-export const mode = writable<GameMode>();
-
-export const letterStates = writable(createLetterStates());
-
-export const settings = writable(createDefaultSettings());
