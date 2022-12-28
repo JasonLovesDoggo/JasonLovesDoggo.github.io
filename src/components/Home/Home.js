addaec882caee7a95510825e743c13669c712d5c
Jason <jasoncameron.all@gmail.com>
2022-12-28 18:23:24 -0500

    I spent way to much time on this.
    feat: added a small little animation show Jason "json" Cameron

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+const TextChanger = () => {
+  const [text, setText] = useState('JASON');
 
-function Home() {
+  const handleMouseEnter = () => {
+    setText('JASON \'JSON\'');
+  }
+
+  const handleMouseLeave = () => {
+    setText('JASON');
+  }
+  const animationPlayState = text === 'JASON \'JSON\'' ? 'paused' : 'initial';
+
+  return (
+    <span style={{animationPlayState: animationPlayState}} className="json" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {text}
+    </span>
+
+  );
+}
+function Home()
+{
   return (
     <section>
       <Container fluid className="home-section" id="home">
@@ -14,15 +33,15 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
+                Hey!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> JASON CAMERON</strong>
+                I am&nbsp;
+                <strong className="main-name"><span className="brackets">&#123;</span><TextChanger/> CAMERON< span className="brackets">&#125;</span></strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
