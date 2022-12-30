48cc29989cc78933d5bda9c5817b491beecb9547
Jason <jasoncameron.all@gmail.com>
2022-12-30 03:03:22 -0500

    removed some debug prints
    feat: added lightbox to images see https://github.com/neptunian/react-photo-gallery/blob/master/examples/src/ExampleWithLightbox.js

--- src/components/Photos/photos.js
+++ src/components/Photos/photos.js
@@ -1,16 +1,28 @@
-import React, {useState} from "react";
+import React, {useCallback, useState} from "react";
 import Gallery from "react-photo-gallery";
 import photos from "./photos.json";
 import CustomPhoto from "./utils.js";
+import Carousel, {Modal, ModalGateway} from 'react-images';
 
 function PhotoGallery() {
     const [categoryFilter, setCategoryFilter] = useState('all');
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, {photo, index}) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
     const handleCategoryChange = event => {
         setCategoryFilter(event.target.value);
     };
 
     const filteredPhotos = photos.filter(photo => {
-        console.log(categoryFilter);
         if (categoryFilter === "all") {
             return true;
         }
@@ -23,19 +35,34 @@ function PhotoGallery() {
                 <h3>My Gallery</h3>
                 <span>Some of the photos that i've taken since <b className="blue">2021</b> </span>
                 <div className="filter-categories">
-                <input onChange={handleCategoryChange} type="radio" id="radioAll" name="radioPhoto" value="all" />
-                <label htmlFor="radioAll">All</label>
-                <input onChange={handleCategoryChange} type="radio" id="radioDog" name="radioPhoto" value="dog"/>
-                <label htmlFor="radioDog">Dogs</label>
-                <input onChange={handleCategoryChange} type="radio" id="radioAssorted" name="radioPhoto"
-                       value="assorted"/>
-                <
-                    label htmlFor="radioAssorted">Assorted</label>
-                <input onChange={handleCategoryChange} type="radio" id="radioNature" name="radioPhoto" value="nature"/>
-                <label htmlFor="radioNature">Nature</label>
+                    <input onChange={handleCategoryChange} type="radio" id="radioAll" name="radioPhoto" value="all"/>
+                    <label htmlFor="radioAll">All</label>
+                    <input onChange={handleCategoryChange} type="radio" id="radioDog" name="radioPhoto" value="dog"/>
+                    <label htmlFor="radioDog">Dogs</label>
+                    <input onChange={handleCategoryChange} type="radio" id="radioAssorted" name="radioPhoto"
+                           value="assorted"/>
+                    <
+                        label htmlFor="radioAssorted">Assorted</label>
+                    <input onChange={handleCategoryChange} type="radio" id="radioNature" name="radioPhoto"
+                           value="nature"/>
+                    <label htmlFor="radioNature">Nature</label>
                 </div>
             </div>
-            <Gallery renderImage={CustomPhoto} photos={filteredPhotos}/>
+            <Gallery renderImage={CustomPhoto} onClick={openLightbox} photos={filteredPhotos}/>
+            <ModalGateway>
+                {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                            currentIndex={currentImage}
+                            views={photos.map(x => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title,
+                            }))}
+                        />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
         </div>
     );
 }
