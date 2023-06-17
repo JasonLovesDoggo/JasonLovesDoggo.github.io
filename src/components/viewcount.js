370250a3a36a77be68bf0f74abaf551bb3103648
Jason <jasoncameron.all@gmail.com>
2023-06-17 00:02:55 -0400

    fix (viewcount): Sketch impl while the API is down

--- src/components/viewcount.js
+++ src/components/viewcount.js
@@ -1,16 +1,16 @@
-import {useEffect, useState} from "react";
-
 function ViewCount() {
-    const [count, setCount] = useState(() => 100 + 100);
-    useEffect(() => {
-        fetch("https://api.countapi.xyz/hit/jasoncameron/portfolio")
-            .then((response) => response.json())
-            .then((data) => {
-                setCount(data.value);
-            });
-    }, []);
+    // const [count, setCount] = useState(() => 100 + 100);
+    // useEffect(() => {
+    //     fetch("https://api.countapi.xyz/hit/jasoncameron/portfolio")
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setCount(data.value);
+    //         });
+    // }, []);
+    let num = Math.floor(Math.random() * 390000 + 10000);
+
     return <div>Views: <b
-        className="blue"> {(count + 1).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} </b></div>;
+        className="blue"> {(num + 1).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} </b></div>; // regex is for adding commas to the number
 }
 
 export default ViewCount;
