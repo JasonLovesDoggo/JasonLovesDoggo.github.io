ba55bbdacd79bc2abe6e37ba2837850c6cd8f1da
Jason <git@jasoncameron.dev>
2024-03-11 20:47:23 -0400

    Fixed outdated linkdin.

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -89,7 +89,7 @@ function Home2() {
                             </li>
                             <li className="social-icons">
                                 <a
-                                    href="https://www.linkedin.com/in/JasonLovesDoggo/"
+                                    href="https://www.linkedin.com/in/jsoncameron/"
                                     target="_blank"
                                     rel="noreferrer"
                                     className="icon-colour  home-social-icons"
