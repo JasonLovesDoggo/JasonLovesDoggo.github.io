d8a4c57011d6d68cae8fddf0c69bc95935176a82
Jason <git@jasoncameron.dev>
2024-03-17 03:09:28 -0400

    Removed todo.

--- src/components/viewcount.js
+++ src/components/viewcount.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 function ViewCount() {
     const [count, setCount] = useState(() => 1 - 1);
     useEffect(() => {
-        fetch("https://abacus.jasoncameron.dev/hit/jasoncameron/portfolio")      // todo initialize to 249039 (which was previously the number of views)
+        fetch("https://abacus.jasoncameron.dev/hit/jasoncameron/portfolio")
             .then((response) => response.json())
             .then((data) => {
                 setCount(data.value);
