d36c1c004e86a92da0942e675d1cee4ff05539f1
Jason <git@jasoncameron.dev>
2024-03-17 03:02:34 -0400

    Deployed a fix for view count using custom count api

--- src/components/viewcount.js
+++ src/components/viewcount.js
@@ -1,16 +1,16 @@
+import {useEffect, useState} from "react";
+
 function ViewCount() {
-    // const [count, setCount] = useState(() => 100 + 100);
-    // useEffect(() => {
-    //     fetch("https://api.countapi.xyz/hit/jasoncameron/portfolio")
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             setCount(data.value);
-    //         });
-    // }, []);
-    let num = 249039//Math.floor(Math.random() * 390000 + 10000);
+    const [count, setCount] = useState(() => 1 - 1);
+    useEffect(() => {
+        fetch("https://abacus.jasoncameron.dev/hit/jasoncameron/portfolio")      // todo initialize to 249039 (which was previously the number of views)
+            .then((response) => response.json())
+            .then((data) => {
+                setCount(data.value);
+            });
+    }, []);
 
     return <div>Views: <b
-        className="blue"> {(num + 1).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} </b></div>; // regex is for adding commas to the number
+        className="blue"> {count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} </b></div>; // regex is for adding commas to the number
 }
-
 export default ViewCount;
