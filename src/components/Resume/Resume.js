7cd0e5ece78d27a5b1674a6d6b08fb0c32a4f715
Jason Cameron <git@jasoncameron.dev>
2024-05-14 14:16:25 +0000

    Updated resume page to better reflect my style

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,11 @@
-import React, {useEffect, useState} from "react";
-import {Container, Row} from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom"
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/resume.pdf";
-import {AiOutlineDownload} from "react-icons/ai";
-import {Document, Page, pdfjs} from "react-pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
 import 'react-pdf/dist/esm/Page/TextLayer.css'
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
@@ -19,37 +20,31 @@ export default function Resume() {
     }, []);
 
     return (<div>
-        <Container fluid className="resume-section">
-            <Row style={{justifyContent: "center", position: "relative"}}>
-                <Button
-                    variant="primary"
-                    href={resumeLink}
-                    target="_blank"
-                    style={{maxWidth: "250px"}}
-                >
-                    <AiOutlineDownload/>
-                    &nbsp;Download CV
-                    </Button>
-                </Row>
-
-                <Row className="resume">
-                    <Document file={pdf} className="d-flex justify-content-center">
-                        <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6}/>
-                    </Document>
-                </Row>
-
-                <Row style={{justifyContent: "center", position: "relative"}}>
+        <Container style={{maxWidth: width + "px"}} fluid className="resume-section">
+            <Row className="resume">
+                <Document file={pdf} className="d-flex justify-content-center">
+                    <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+                </Document>
+            </Row>
+            <Row style={{ justifyContent: "space-evenly", position: "relative" }}>
+                <Col style={{ disply: 'flex' }}>
                     <Button
                         variant="primary"
                         href={resumeLink}
                         target="_blank"
-                        style={{maxWidth: "250px"}}
+                        className="resume-download"
                     >
-                        <AiOutlineDownload/>
+                        <AiOutlineDownload />
                         &nbsp;Download CV
-                    </Button>
-                </Row>
-            <Particle/>
+                    </Button></Col>
+                <Col style={{ display: "flex", paddingRight: "5vw" }}>By the way, I'm written in LaTeX & open sourced if you want to&nbsp;<Link
+                    style={{ textDecoration: "none", fontWeight: "bolder", color: "4b8bd5" }}
+                    to="https://github.com/JasonLovesDoggo/JasonLovesDoggo.github.io/blob/main/src/Assets/resume.tex"
+                    target="_blank">view me</Link></Col>
+
+            </Row>
+
+            <Particle />
         </Container>
     </div>);
 }
