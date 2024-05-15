9bf0859afea6ce2b3f1eb1d319825a2aea5337a1
Jason Cameron <git@jasoncameron.dev>
2024-05-15 18:46:45 +0000

    add responsitivity to resume page

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useLayoutEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom"
@@ -12,13 +12,24 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 const resumeLink = "https://github.com/JasonLovesDoggo/JasonLovesDoggo.github.io/raw/main/src/Assets/resume.pdf";
 
-export default function Resume() {
-    const [scale, setScale] = useState(1.7);
 
-    useEffect(() => {
-        setScale(window.innerWidth > 786 ? 1.7 : 0.6);
+function useGetScale() {
+    const [scale, setScale] = useState(1.7);
+    useLayoutEffect(() => {
+        function updateScale() {
+            setScale(window.innerWidth > 786 ? 1.7 : 0.6);
+        }
+        window.addEventListener('resize', updateScale);
+        updateScale();
+        return () => window.removeEventListener('resize', updateScale);
     }, []);
-    console.log(scale)
+    return scale;
+}
+
+
+export default function Resume() {
+
+    const scale = useGetScale();
     return (<div>
         <Container fluid className="resume-section">
             <Row className="resume">
@@ -26,7 +37,7 @@ export default function Resume() {
                     <Page pageNumber={1} scale={scale} />
                 </Document>
             </Row>
-            <Row className="resume-text-row" style={{ maxWidth: `calc(${scale} * 612px)` }}>
+            <Row className="resume-text-row">
 
                 <Col className="resume-text">By the way, I'm written in LaTeX & open sourced if you want to&nbsp;<Link
                     style={{ textDecoration: "none", fontWeight: "bolder", color: "4b8bd5" }}
