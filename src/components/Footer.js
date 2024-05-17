f9929fdc88e0167dc910507dc78601d90ab91a6e
Jason Cameron <git@jasoncameron.dev>
2024-05-17 00:34:17 +0000

    Redid footer completly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 import React from "react";
-import {Col, Container, Row} from "react-bootstrap";
-import {AiFillGithub, AiOutlineTwitter,} from "react-icons/ai";
-import {FaLinkedinIn, FaServer} from "react-icons/fa";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+    AiFillGithub,
+    AiOutlineTwitter,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 import ViewCount from "./viewcount";
 import Wakatime from "./wakatime";
 
@@ -10,55 +13,58 @@ function Footer() {
     let year = date.getFullYear();
     return (
         <Container fluid className="footer">
-            <Row>
-                <Col md="4" className="footer-copywright">
-                    <h3>Developed by Jason Cameron</h3>
-                </Col>
-                <Col md="4" className="footer-copywright">
-                    <h3>Copyright © {year} Jason Cameron</h3>
-                </Col>
+            <Row style={{ placeItems: "center" }}>
                 <Col md="4" className="footer-body">
                     <ul className="footer-icons">
                         <li className="social-icons">
                             <a
                                 href="https://github.com/JasonLovesDoggo"
-                                style={{color: "white"}}
+                                style={{ color: "white" }}
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
-                                <AiFillGithub/>
+                                <AiFillGithub />
                             </a>
                         </li>
                         <li className="social-icons">
                             <a
                                 href="https://twitter.com/JasonLovesDoggo"
-                                style={{color: "white"}}
+                                style={{ color: "white" }}
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
-                                <AiOutlineTwitter/>
+                                <AiOutlineTwitter />
                             </a>
                         </li>
                         <li className="social-icons">
                             <a
                                 href="https://www.linkedin.com/in/jsoncameron/"
-                                style={{color: "white"}}
+                                style={{ color: "white" }}
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
-                                <FaLinkedinIn/>
+                                <FaLinkedinIn />
                             </a>
                         </li>
-                        <li style={{color: "white"}} className="social-icons">
-                            <ViewCount/>
+                        <li className="social-icons">
                         </li>
-                        <li style={{color: "white"}} className="social-icons">
-                            <Wakatime/>
+                    </ul>
+                </Col>
+                <Col md="4" className="footer-copywright">
+                    <h3>Copyright © {year} Jason Cameron</h3>
+                </Col>
+                <Col md="4" className="footer-copywright">
+                    <ul className="footer-icons">
+                        <li className="social-icons">
+                            <ViewCount />
                         </li>
-                        <li className="social-icons status">
+                        <li className="social-icons">
+                            <Wakatime />
+                        </li>
+                        {/* <li className="social-icons status">
                             <a href="https://status.jasoncameron.dev" target="_blank" rel="noopener noreferrer"
                                style={{color: "white", textDecoration: "none"}}><FaServer/>&nbsp;&nbsp;<span style={{textAlign: "center"}}>Status</span></a>
-                        </li>
+                        </li> */}
 
                     </ul>
                 </Col>
