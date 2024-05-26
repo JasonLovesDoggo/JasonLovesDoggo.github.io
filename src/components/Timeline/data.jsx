a5e1b08de462022ba38531afde93247f54cd2185
Jason <git@jasoncameron.dev>
2024-05-26 14:28:09 -0400

    Fix icons

--- src/components/Timeline/data.jsx
+++ src/components/Timeline/data.jsx
@@ -1,16 +1,18 @@
 import {TbBrandGithub} from "react-icons/tb";
+import {GoBook} from "react-icons/go";
+import {FaChild} from "react-icons/fa6";
 
 
 const timelineData = [
     {
         content: "I Open up a random piece of (I belive C#) code because it looks interesting",
         date: "Mid 2018",
-        icon: "👶"
+        icon: <FaChild/>
     },
     {
         content: 'I start learning what coding is and that it exists; my grandma buys me a "How the world works" book - I\'m hooked',
         date: "2019",
-        icon: "📚"
+        icon: <GoBook/>
     },
     {content: "I joined GitHub", date: "June 2020", icon: <TbBrandGithub/>}
 ];
