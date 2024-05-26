e9f3ca8489ecfda156a8357fc5890653e42a1ded
Jason <git@jasoncameron.dev>
2024-05-26 14:28:23 -0400

    Fix MUI theming

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -4,17 +4,20 @@ import Particle from "../Particle";
 import {Container} from "react-bootstrap";
 import timelineData from "./data";
 import TimelineCard, {Theme} from "./TimelineCard";
+import {ThemeProvider} from "@mui/material";
 
 
 export default function ProgrammingTimeline() {
     return (
-        <Container className="timeline-container" style={{minHeight: "100vh"}} fluid>
-            <Timeline position="alternate" theme={Theme}>
-                {timelineData.map((event) => (
-                    TimelineCard(event.date, event.content, event.icon)
-                ))}
-            </Timeline>
-            <Particle/>
-        </Container>
+        <ThemeProvider theme={Theme}>
+            <Container className="timeline-container" style={{minHeight: "100vh"}} fluid>
+                <Timeline position="alternate">
+                    {timelineData.map((event) => (
+                        TimelineCard(event.date, event.content, event.icon)
+                    ))}
+                </Timeline>
+                <Particle/>
+            </Container>
+        </ThemeProvider>
     );
 }
