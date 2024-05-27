5830b9103c8efbe4d5ffd335c917445059b0f064
Jason <git@jasoncameron.dev>
2024-05-26 20:33:01 -0400

    Update Timeline cards using MUI

--- src/components/Timeline/TimelineCard.jsx
+++ src/components/Timeline/TimelineCard.jsx
@@ -5,7 +5,7 @@ import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import * as React from "react";
 import {TimelineOppositeContent} from "@mui/lab";
-import {Box, createTheme, Typography} from "@mui/material";
+import {createTheme, Paper, Typography} from "@mui/material";
 import {indigo} from '@mui/material/colors';
 import Button from '@mui/material/Button';
 import hash from './hasher';
@@ -20,41 +20,36 @@ export const Theme = createTheme({
     },
 });
 
-const cyrb53 = (str, seed = 1) => {
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for (let i = 0, ch; i < str.length; i++) {
-        ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
-    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
-    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-
-    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
-};
 export default function TimelineCard(date, content, icon, position, links) {
     return (
-        <TimelineItem key={cyrb53(content)}>
-            <TimelineOppositeContent color="primary">
-                <Typography component="h4" color="primary">
-                    <Box fontWeight='bold' display='inline'>{date}</Box></Typography>
+        <TimelineItem key={hash(content)}>
+            <TimelineOppositeContent sx={{margin: 'auto 0'}} style={{float: position}} color="primary" varient="body2">
+                <b>{date}</b>
             </TimelineOppositeContent>
+
+
             <TimelineSeparator>
+                <TimelineConnector/>
                 <TimelineDot color="primary" variant="filled">
                     {icon}
                 </TimelineDot>
                 <TimelineConnector/>
             </TimelineSeparator>
-            <TimelineContent className="timeline-content">
-                <div>
-                    <Typography maxWidth="30vw" sx={{float: position}} variant="body1" color="primary">
+            {/*<TimelineSeparator>*/}
+            {/*    <TimelineConnector>*/}
+            {/*    <TimelineDot color="primary" variant="filled">*/}
+            {/*        {icon}*/}
+            {/*    </TimelineDot>*/}
+            {/*    </TimelineConnector>*/}
+            {/*</TimelineSeparator>*/}
+            <TimelineContent>
+                <Paper style={{float: position}} square={false} elevation={3} sx={{padding: '6px 16p'}} className="timeline-content-container">
+                    <Typography align="left" className="timeline-content" maxWidth="35vw" variant="body1">
                         {content}
                     </Typography>
                     {links && links.map((data) => (
-                    <Button sx={{background: data.color}} variant="contained" target="_blank" href={data.url}>{data.text}</Button>))}
-                </div>
+                    <Button className="timeline-button" sx={{background: data.color}} variant="contained" target="_blank" href={data.url}>{data.text}</Button>))}
+                </Paper>
             </TimelineContent>
         </TimelineItem>
 
