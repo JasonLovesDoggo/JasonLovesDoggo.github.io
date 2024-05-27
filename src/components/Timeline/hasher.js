62c9b9fc96ced62e287f8f1f7bcb56f79425a44d
Jason <git@jasoncameron.dev>
2024-05-27 01:22:08 -0400

    Rewrote glass to be returning objects

--- src/components/Timeline/hasher.js
+++ src/components/Timeline/hasher.js
@@ -1,4 +1,4 @@
-const cyrb53 = (str, seed = 1) => {
+const cyrb53 = (str, seed = 0) => {
     let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
     for (let i = 0, ch; i < str.length; i++) {
         ch = str.charCodeAt(i);
--- src/components/utils/Glass.js
+++ src/components/utils/Glass.js
@@ -1,18 +1,9 @@
-import {css} from "@emotion/react"; // Or your preferred CSS-in-JS library
-
 export default function generateGlassmorphismStyle({transparency, color, blur, outline}) {
-    const outlineCSS = outline
-        ? `1px solid rgba(${color.r}, ${color.g}, ${color.b}, ${outline})`
-        : "none"; // Default to no outline
-
-    return css`
-        /* Begin Auto-Generated Glassmorphism Styles */
-        background: rgba(${color.r}, ${color.g}, ${color.b}, ${transparency});
-        border-radius: 16px;
-        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-        backdrop-filter: blur(${blur}px);
-        -webkit-backdrop-filter: blur(${blur}px);
-        border: ${outlineCSS};
-        /* End Auto-Generated Glassmorphism Styles */
-    `;
+    return {
+        background: `rgba(${color.r}, ${color.g}, ${color.b}, ${transparency})`,
+        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+        backdropFilter: `blur(${blur}px)`,
+        WebkitBackdropFilter: `blur(${blur}px)`, // For older Safari versions
+        border: outline ? `1px solid rgba(${color.r}, ${color.g}, ${color.b}, ${outline})` : "none",
+    };
 }
