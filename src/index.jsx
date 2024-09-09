10bb789541e5ab75e00953eec2d7afc8f990abb0
Jason Cameron <git@jasoncameron.dev>
2024-09-08 21:05:10 -0400

    Fixed build issues

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<DevSupport ComponentPreviews={ComponentPreviews}
-                        useInitialHook={useInitial}
->
+root.render(
     <App/>
-</DevSupport>);
+);
